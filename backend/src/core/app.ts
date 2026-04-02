883562c303cdefe3423035ab8055449ef43f4c8c
gameshler <rogue@sentinel.localdomain>
2026-04-02 18:13:00 +0300

    initial commit

---
+++ backend/src/core/app.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
